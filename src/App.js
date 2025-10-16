f1c48783e5a3990a16e0de47636df0c4ddff555f
jeelpatel22 <130308961+jeelpatel22@users.noreply.github.com>
2025-10-16 19:01:32 -0400

    Fix ESLint errors for vercel deployment

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import ParticleBackground from './components/ParticleBackground';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
